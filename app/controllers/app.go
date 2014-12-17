faf0f46ac0e3540042ed8c940ab9cb42b261b035
DedoCibula <dedo.cibula@gmail.com>
2014-12-17 04:25:53 +0100

    reverted to manual insertion :/

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -15,13 +15,12 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Lesson(id string) revel.Result {
+func (c App) Sense() revel.Result {
 	address := rm.LoadESAddress() + rm.LoadESPort() + "/_plugin/marvel/sense/index.html"
-	param := rm.LoadESAddress() + "/App/Material/" + id
-	return c.Redirect("%s?load_from=%s", address, param)
+	return c.Redirect(address)
 }
 
-func (c App) Material(id int) revel.Result {
+func (c App) Lesson(id int) revel.Result {
 	str, err := rm.LoadMaterial(id)
 	if err != nil {
 		return c.NotFound("Couldn't find specified file")
