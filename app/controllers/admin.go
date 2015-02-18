e01340ffd9864246ac5c40a054a08a437600277d
DedoCibula <dedo.cibula@gmail.com>
2015-02-18 22:11:39 +0100

    added Moustache, implemented admin page results, added admin controller, fixed typos

--- app/controllers/admin.go
+++ app/controllers/admin.go
@@ -7,11 +7,21 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-type Websocket struct {
+type Admin struct {
 	*revel.Controller
 }
 
-func (c Websocket) Records(ws *websocket.Conn) revel.Result {
+func (c Admin) Index() revel.Result {
+	return c.Render()
+}
+
+func (c Admin) ResultEndpoint() revel.Result {
+	qm := models.NewQuizManager()
+	results := qm.GetResults()
+	return c.RenderJson(results)
+}
+
+func (c Admin) RecordEndpoint(ws *websocket.Conn) revel.Result {
 	subscription := models.QuizMonitorInstance().Subscribe()
 	defer models.QuizMonitorInstance().Unsubscribe(subscription)
 
